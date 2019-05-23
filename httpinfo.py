bd51aa90f30454750614c0b6da8ee0270301b239
azdhebar <36300596+azdhebar@users.noreply.github.com>
2019-05-24 00:28:23 +0530

    Add files via upload

---
+++ httpinfo.py
@@ -0,0 +1,17 @@
+import socket
+from colorama import *
+s = socket.socket()
+s.settimeout(2)
+print(Fore.GREEN+"Please Enter Web Address:\n")
+target = raw_input(Fore.WHITE)
+
+try:    
+    s.connect((target,80))
+    s.send('HEAD / HTTP\nHost:'+target+'\n\n')
+    print(Fore.YELLOW)    
+    print(s.recv(1024))
+except:
+    print(Fore.RED+"Web Address is Wrong or Offline")
+    
+s.close()
+ 
